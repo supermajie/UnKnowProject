a4fe03c99b19159adad30795915c35ce7ccd1673
supermajie <super_majie@126.com>
2017-12-20 19:40:31 +0800

    Update UnknowGameBuild.cs

--- Source/UnknowGame/UnknowGameBuild.cs
+++ Source/UnknowGame/UnknowGameBuild.cs
@@ -6,6 +6,6 @@ public class UnknowGame : ModuleRules
 {
 	public UnknowGame(TargetInfo Target)
 	{
-		Public DependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 	}
 }
