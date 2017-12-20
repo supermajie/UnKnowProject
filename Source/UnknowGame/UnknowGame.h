52b8d07aa3f271f684a25c3ee4cc3faf73ff2eb9
supermajie <super_majie@126.com>
2017-12-20 19:33:01 +0800

    Create UnknowGame.h

---
+++ Source/UnknowGame/UnknowGame.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __BRICKGAME_H__
+#define __BRICKGAME_H__
+
+#include "Engine.h"
+
+#define COLLISION_PROJECTILE	ECC_GameTraceChannel1
+
+
+#endif
