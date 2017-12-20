4a2c1f1f47e08679139de9720ee5410c23cf6bdf
supermajie <super_majie@126.com>
2017-12-20 19:30:21 +0800

    Delete UnKnowGameEditorTarget.cs

--- UnKnowGameEditorTarget.cs
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-using UnrealBuildTool;
-using System.Collections.Generic;
-
-public class UnKnowGameEditorTarget : TargetRules
-{
-  public UnKnowGameEditorTarget(TargetInfo Target)
-}
